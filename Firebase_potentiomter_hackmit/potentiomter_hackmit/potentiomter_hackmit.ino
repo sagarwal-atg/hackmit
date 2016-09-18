9be6b596c78ba6ae9f2ce854626098484e7cce0b
Milo Knowles <mknowles@mit.edu>
2016-09-18 01:43:03 -0400

    Added hall effect RPM calculations

--- Firebase_potentiomter_hackmit/potentiomter_hackmit/potentiomter_hackmit.ino
+++ Firebase_potentiomter_hackmit/potentiomter_hackmit/potentiomter_hackmit.ino
@@ -43,6 +43,7 @@ void setup() {
 
 */
 
+//POTENTIOMETER STUFF
 int potPin = A0;    // select the input pin for the potentiometer
 int ledPin = 13;   // select the pin for the LED
 double potRead = 0;       // variable to store the value coming from the sensor
@@ -50,14 +51,37 @@ double angle = 0;
 double potCenterRead = 591;
 
 
+//HALL EFFECT STUFF
+int halfRevs;
+float rpm;
+unsigned long timeold;
+int hallEffectPin = 0;
+
+
+//This function is called whenever a magnet/interrupt is detected by the arduino
+ void magnet_detect()
+ {
+  //increment the # of half revolutions
+   halfRevs++;
+ }
+
+
 void setup() {
   pinMode(ledPin, OUTPUT);  // declare the ledPin as an OUTPUT
   Serial.begin(9600);      // open the serial port at 115200 bps:    
+
+  //Initialize the intterrupt pin (Arduino digital pin 2)
+   attachInterrupt(hallEffectPin, magnet_detect, RISING);
+   
+   halfRevs = 0;
+   rpm = 0;
+   timeold = 0;
 }
 
 void loop() {
+
+  //GET VALUE FROM THE POTENTIOMETER
   potRead = analogRead(potPin);  // read the value from the sensor
-  
   if (potRead >= potCenterRead)  // normalizing the potentiometer value to get the angle 
      {  potRead = potRead - potCenterRead ; 
         angle = potRead/511; 
@@ -69,11 +93,26 @@ void loop() {
        angle = potRead/511;
        angle = -angle * 110;     
     }
-   Serial.print("angle: ");
-   Serial.print(angle);
-   Serial.println();
-    
+
+  //CALCULATE RPMs
+  if (halfRevs >= 2) { 
+     rpm = float(halfRevs) * 30000 / (millis()-timeold); //30000 because divide revs by 2
+     timeold = millis();
+     halfRevs = 0;
+     }
+     
+
+  //DEBUGGING AREA
+  Serial.print("Angle: ");
+  Serial.print(angle);
+  Serial.print(" | ");
+  Serial.print("RPM: ");
+  Serial.print(rpm);
+  Serial.print('\n');
+
+  //blink the status LED
   digitalWrite(ledPin, HIGH);  // turn the ledPin on
   delay(10);              // stop the program for some time
   digitalWrite(ledPin, LOW);   // turn the ledPin off
 }
+
