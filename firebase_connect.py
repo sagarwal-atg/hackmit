44a7ced00ddb012aaebee5090ee9e3acac65938f
shubhankar12 <shubhankar0109@gmail.com>
2016-09-18 07:01:28 -0400

    pyrebase

--- firebase_connect.py
+++ firebase_connect.py
@@ -4,8 +4,10 @@ new_user = 'Ozgur Vatansever'
 
 i = 0
 while True:
-    result_angle = firebase.post('/angle', data={"angle":i * 10 , "rpm":i * 20} , params={'print': 'pretty'})
+    result_angle = firebase.post('/angle', data={"angle":i * 10 } , params={'print': 'pretty'})
+    result_angle = firebase.post('/rpm', data={ "rpm":i * 20} , params={'print': 'pretty'})
     i+=1
-print result_angle
+    print result_angle
+
 #print result_rpm
 print "Success"
